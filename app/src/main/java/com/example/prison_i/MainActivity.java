9e5f54096a3bf9c0a341fde14ace4d96ae91d5ee
akash <akash2237778@gmail.com>
2019-05-17 00:07:02 +0530

    login introduced needs improvement (keyValue)

--- app/src/main/java/com/example/prison_i/MainActivity.java
+++ app/src/main/java/com/example/prison_i/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.prison_i;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent LoginIntent;
+
+
+    public void startActivity(View view){
+        startActivity(LoginIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoginIntent = new Intent(getApplicationContext(),Login_Activity.class);
     }
 }
